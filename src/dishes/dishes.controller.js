b9cbe701126581312df8f343f568d45737b4ab3c
Mitch Bishop <Mitch.Bishop@NGIC.COM>
2024-02-21 21:28:03 -0500

    New repo.

---
+++ src/dishes/dishes.controller.js
@@ -0,0 +1,107 @@
+const path = require("path");
+
+// Use the existing dishes data
+const dishes = require(path.resolve("src/data/dishes-data"));
+
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
+function pricePropertyIsValid(req, res, next) {
+    const { data: { price } = {} } = req.body;
+    if (price > 0 && price != 17) {
+        return next();
+    }
+    next({
+        status: 400,
+        message: `Value of the 'price' property must be greater than zero. Received: ${price}`,
+    });
+}
+
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name: name,
+    description: description,
+    price: price,
+    image_url: image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+}
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function dishExists(req, res, next) {
+  const dishId = req.params.dishId;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    return next();
+  }
+  next({
+    status: 404,
+    message: `dish id not found: ${req.params.dishId}`,
+  });
+}
+
+function read(req, res) {
+  const dishId = req.params.dishId;
+  const foundDish = dishes.find((dish) => (dish.id = dishId));
+  res.json({ data: foundDish });
+}
+
+function update(req, res, next) {
+  const dishId = req.params.dishId;
+  const { data: { id, name, description, price, image_url } = {} } = req.body;
+  if(id && id !== dishId) {
+    return next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+    });
+  }
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  foundDish.id = dishId;
+  foundDish.name = name;
+  foundDish.description = description;
+  foundDish.price = price;
+  foundDish.image_url = image_url;
+
+  res.json({ data: foundDish });
+}
+
+module.exports = {
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    pricePropertyIsValid,
+    create,
+  ],
+  list,
+  read: [dishExists, read],
+  update: [
+    dishExists,
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    pricePropertyIsValid,
+    update,
+  ],
+  dishExists,
+};
---
+++ src/orders/orders.controller.js
@@ -0,0 +1,150 @@
+const path = require("path");
+
+// Use the existing order data
+const orders = require(path.resolve("src/data/orders-data"));
+
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+const { forEach } = require("../data/orders-data");
+
+// TODO: Implement the /orders handlers needed to make the tests pass
+function statusPropertyIsValid(req, res, next) {
+  const { data: { status, dishes } = {} } = req.body;
+  const thisStatus = dishes[0].status ? dishes[0].status : status;
+  if (!thisStatus || thisStatus === "invalid") {
+    return next({
+      status: 400,
+      message: `Value of the 'status' must be valid. Received: ${status}`,
+    });
+  }
+  next();
+}
+
+function dishesPropertyIsValid(req, res, next) {
+  const { data: { dishes } = {} } = req.body;
+  if (dishes == 0 || !Array.isArray(dishes)) {
+    return next({
+      status: 400,
+      message: `Value of the 'dishes' property must be a non-empty array. Received: ${dishes}`,
+    });
+  }
+  dishes.forEach((dish, index) => {
+    if (
+      !dish.quantity ||
+      dish.quantity <= 0 ||
+      !Number.isInteger(dish.quantity)
+    ) {
+      return next({
+        status: 400,
+        message: `Dish ${index} quantity is not valid.`,
+      });
+    }
+  });
+  next();
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (!data[propertyName]) {
+      return next({ status: 400, message: `Must include a ${propertyName}` });
+    }
+    next();
+  };
+}
+
+function create(req, res) {
+  const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
+
+  const newOrder = {
+    id: nextId(),
+    deliverTo: deliverTo,
+    mobileNumber: mobileNumber,
+    status: status ? status : "pending",
+    dishes: dishes,
+  };
+  orders.push(newOrder);
+  res.status(201).json({ data: newOrder });
+}
+
+function destroy(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  if (foundOrder.status !== "pending") {
+    return next({
+      status: 400,
+      message: `Order status must be pending. Status: ${foundOrder.status}.`,
+    });
+  }
+  if (orders.indexOf(foundOrder) > -1) {
+    orders.splice(orders.indexOf(foundOrder), 1);
+  }
+  res.sendStatus(204);
+}
+
+function list(req, res) {
+  res.json({ data: orders });
+}
+
+function orderExists(req, res, next) {
+  const orderId = req.params.orderId;
+  const foundOrder = orders.find((order) => order.id === orderId);
+  if (foundOrder) {
+    return next();
+  }
+  next({
+    status: 404,
+    message: `order id not found: ${req.params.orderId}`,
+  });
+}
+
+function read(req, res) {
+  const orderId = req.params.orderId;
+  const foundOrder = orders.find((order) => (order.id = orderId));
+  res.json({ data: foundOrder });
+}
+
+function update(req, res, next) {
+  const orderId = req.params.orderId;
+  const { data: { id, deliverTo, mobileNumber, status, dishes } = {} } =
+    req.body;
+  if (id && id !== orderId) {
+    return next({
+      status: 400,
+      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`,
+    });
+  }
+  const foundOrder = orders.find((order) => order.id === orderId);
+
+  foundOrder.id = orderId;
+  foundOrder.deliverTo = deliverTo;
+  foundOrder.mobileNumber = mobileNumber;
+  foundOrder.status = status ? status : "pending";
+  foundOrder.dishes = dishes;
+
+  res.json({ data: foundOrder });
+}
+
+module.exports = {
+  create: [
+    bodyDataHas("deliverTo"),
+    bodyDataHas("mobileNumber"),
+    bodyDataHas("dishes"),
+    dishesPropertyIsValid,
+    statusPropertyIsValid,
+    create,
+  ],
+  list,
+  read: [orderExists, read],
+  update: [
+    orderExists,
+    bodyDataHas("deliverTo"),
+    bodyDataHas("mobileNumber"),
+    bodyDataHas("dishes"),
+    dishesPropertyIsValid,
+    statusPropertyIsValid,
+    update,
+  ],
+  delete: [orderExists, destroy],
+  orderExists,
+};
